99dfa268fbd96af6b8493ac1d6d9cd9364b70039
Naoo122 <kingfishernao2017@gmail.com>
2025-10-17 18:14:18 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+// メニュー開閉
+const Menutab = document.getElementById('menutab');
+const Menuicon = document.getElementById('menuicon');
 
+if (Menuicon && Menutab) {
+  Menuicon.addEventListener('click', () => {
+    Menutab.classList.toggle('active');
+  });
+}
+
+window.batsu = () => {
+  if (Menutab) Menutab.classList.remove('active');
+};
