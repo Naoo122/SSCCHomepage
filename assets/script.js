284251573a629bc8bbdb39e36ee27701945a6e6a
Naoo122 <kingfishernao2017@gmail.com>
2025-10-17 18:14:56 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,14 @@ if (Menuicon && Menutab) {
 window.batsu = () => {
   if (Menutab) Menutab.classList.remove('active');
 };
+
+// 共通ヘッダーとフッターの読み込み
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/shared/header.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("header").innerHTML = data);
+
+  fetch("/shared/footer.html")
+    .then(res => res.text())
+    .then(data => document.getElementById("footer").innerHTML = data);
+});
